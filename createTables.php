c4f3c92e7aa880a094491bbb87bff4b3d208d246
Nikita <nik.findyruv@mail.ru>
2023-03-26 23:45:17 +0700

    new commit

---
+++ createTables.php
@@ -0,0 +1,27 @@
+<?php
+$serevername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "base";
+
+$link = new mysqli($serevername, $username, $password, $dbname);
+
+if($link->connect_error){
+    die("Connection failed: " . $link->connect_error);
+}
+
+$sql = "CREATE TABLE users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR (30) NOT NULL,
+        surname VARCHAR (30) NOT NULL,
+        password VARCHAR (30) NOT NULL)";
+
+if($link->query($sql) === TRUE){
+    print("Table users created successfully");
+}
+else {
+    print("Error" . $link->error);
+}
+
+$link->close();
+?>
--- myPhp.php
+++ myPhp.php
@@ -5,13 +5,22 @@ $password = "";
 
 $link = new mysqli($serevername, $username, $password);
 
-if($link == false){
+if($link->connect_error){
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
 }
 else {
     print("Соединение установлено успешно");
 }
 
+$sql = "CREATE DATABASE base";
 
+if($link->query($sql)===TRUE){
+    print("Database created successfully");
+}
+else {
+    print("Error" . mysqli_connect_error());
+}
+
+$link->close();
 ?>
 
