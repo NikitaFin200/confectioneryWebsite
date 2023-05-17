9c6e5ac49754b905cf73d7808b7b809dedc1c0f8
Nikita <nik.findyruv@mail.ru>
2023-05-17 21:14:41 +0700

    new

---
+++ myPhp.php
@@ -0,0 +1,13 @@
+<?php
+$serevername="localhost";
+$username="root";
+$password="";
+
+$link = new mysqli($serevername, $username, $password);
+if($link == false){
+    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+}
+else {
+    print("Соединение установлено успешно");
+}
+?>
